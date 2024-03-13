dd0a94f93f67df89b9f6ffd4ed5508fb498c1d6f
kukuhtri1999 <kukuhthewow@gmail.com>
2024-03-13 15:21:02 +0700

    install gin framework

--- cmd/golearnhub/main.go
+++ cmd/golearnhub/main.go
@@ -1,8 +1,19 @@
 // cmd/golearnhub/main.go
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-    fmt.Println("Hello, GoLearnHub!")
+    // Initialize a GIN router
+    router := gin.Default()
+
+    // Define a simple route
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Hello, GoLearnHub!",
+        })
+    })
+
+    // Run the server on port 8080
+    router.Run(":8080")
 }
